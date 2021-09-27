0621383b9e7ab5f261f90a261bafa7b5843ae36d
hongyingaliu <88841145+hongyingaliu@users.noreply.github.com>
2021-09-27 01:56:13 -0400

    Update joystick_test.py

--- interactive devices/joystick_test.py
+++ interactive devices/joystick_test.py
@@ -5,15 +5,15 @@ button2 = Button(27)
 button3 = Button(17)
 
 while True:
-  if button1.is_pressed:
-    print(“sw is pressed")
-    #wait for release or else it will keep going
-    button1.wait_for_release()      
-  elif button2.is_pressed:
-    print(“y is pressed")
-    button2.wait_for_release() 
+	if button1.is_pressed:
+		print("SW is pressed)
+		button1.wait_for_release()
+	elif button2.is_pressed:
+		print("y is pressed)
+		button2.wait_for_release()
 	elif button3.is_pressed:
-	  print(“x is pressed”)
-    button3.wait_for_release() 
+		print("x is pressed)
+		button3.wait_for_release()
+		      
 
           
