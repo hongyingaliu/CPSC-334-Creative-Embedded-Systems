aee9eda20e65103ae0c9fc8dde5b446c70278d0a
hongyingaliu <88841145+hongyingaliu@users.noreply.github.com>
2021-10-06 23:40:41 -0400

    Create test_input.ino

---
+++ interactive devices/task2/test_input.ino
@@ -0,0 +1,54 @@
+const int joy_sw = 18;
+const int joy_x = 15;
+const int joy_y = 2;
+const int button = 4;
+
+int xpos = 0;
+int oldxpos = 0;
+int ypos = 0;
+int oldypos = 0;
+int mapX = 0;
+int mapY = 0;
+int sw_state = 1;
+int oldSwState = 0;
+int buttonState = 1;
+int oldButtonState = 0;
+
+void printConditions(){
+    mapX = map(xpos, 0, 1023, -512, 512);
+    mapY = map(ypos, 0, 1023, -512, 512);
+    Serial.print(buttonState);
+    Serial.print(" "); 
+    Serial.print(sw_state); 
+    Serial.print(" "); 
+    Serial.print(mapX);
+    Serial.print(" ");
+    Serial.print(mapY);
+    Serial.print("\n");   
+}
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(9600);
+  pinMode(joy_x, INPUT);
+  pinMode(joy_y, INPUT);
+  pinMode(joy_sw, INPUT_PULLUP);
+  pinMode(button, INPUT_PULLUP);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  buttonState = digitalRead(button);
+  xpos = analogRead(joy_x);
+  ypos = analogRead(joy_y);
+  sw_state = digitalRead(joy_sw);
+  if(buttonState != oldButtonState || sw_state != oldSwState || oldxpos != xpos || oldypos != ypos){
+    printConditions();
+    oldButtonState = buttonState;
+    sw_state = oldSwState;
+    oldxpos = xpos;
+    oldypos = ypos;
+  }
+
+  delay(100);
+
+}
