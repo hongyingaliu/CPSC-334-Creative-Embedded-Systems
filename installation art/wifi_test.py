5f8ade3576e1cd87af38728aae37172289aa4e1e
hongyingaliu <88841145+hongyingaliu@users.noreply.github.com>
2021-10-26 18:36:24 -0400

    added ability to send more than one input to sc3

--- installation art/wifi_test.py
+++ installation art/wifi_test.py
@@ -14,18 +14,20 @@ while True:
     sc_content = 0
 
     while True:
-        content = client.recv(32)
+        content = (client.recv(1024)).decode("utf-8")
 
         if len(content) == 0:
             break
         else:
             sc_content = content
-            print(content)
+            print(sc_content)
     print("closing connection")
     client.close()
 
-    sc_content = sc_content.decode("utf-8")
-    print(sc_content)
+    all_input = sc_content.split(" ")
+    l1 = all_input[0]
+    l2 = all_input[1]
+
     parser = argparse.ArgumentParser()
     parser.add_argument("--ip", default="172.29.21.84",
                         help="The ip of the OSC server")
@@ -33,11 +35,5 @@ while True:
                         help="The port the OSC server is listening on")
     args = parser.parse_args()
     sc_client = udp_client.SimpleUDPClient(args.ip, args.port)
-    sc_client.send_message("/light", sc_content)
-    #msg = osc_message_builder.OscMessageBuilder(address = '/sensors')
-    #msg.add_arg(int(sc_content), arg_type='i')
-    #msg.add_arg('light', arg_type='s')
-    #msg = msg.build()
-    #sc_client.send(msg)
-    #print("message sent")
-    #time.sleep(0.1);
+    sc_client.send_message("/light1", int(l1))
+    sc_client.send_message("/light2", int(l2))
