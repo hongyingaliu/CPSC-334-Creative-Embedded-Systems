fcc1e49291dfcbb408236b018e37d9c30de35e00
hongyingaliu <88841145+hongyingaliu@users.noreply.github.com>
2021-10-26 23:53:37 -0400

    all sensors included

--- installation art/sensor_input.ino
+++ installation art/sensor_input.ino
@@ -1,3 +1,5 @@
+// capacitive sensing includes
+#include <CapacitiveSensor.h>
 #include <WiFi.h>
 
 const char* ssid = "yale wireless";
@@ -7,6 +9,11 @@ const char* host = "172.29.21.84";
 
 const int lightPinOne = 35;
 const int lightPinTwo = 34;
+const int lightPinThree = 32;
+const int lightPinFour = 33;
+const int piezo_pin = 39;
+CapacitiveSensor sensor = CapacitiveSensor(18,19);
+
 void setup() {
   // put your setup code here, to run once:
   Serial.begin(115200);
@@ -32,15 +39,18 @@ void loop() {
   WiFiClient client;
   int light_one = analogRead(lightPinOne);
   int light_two = analogRead(lightPinTwo);
-
+  int light_three = analogRead(lightPinThree);
+  int light_four = analogRead(lightPinFour);
+  int piezo_state = analogRead(piezo_pin);
+  long touch =  sensor.capacitiveSensor(30);  
   if (!client.connect(host, port)) {
       Serial.println("connection failed");
       return;
   }
 
-  client.print(String(light_one) + " " + String(light_two));
+  client.print(String(light_one) + " " + String(light_two) + " " + String(light_three) + " " + String(light_four) + " " +String(piezo_state) + " " + String(touch));
   //client.print(light_two);
   //Serial.println(light_val);
-  delay(10);
+  delay(5);
   
 }
