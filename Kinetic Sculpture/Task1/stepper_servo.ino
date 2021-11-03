93d594ed3b2180206be1da4aa665569201dcd95c
hongyingaliu <88841145+hongyingaliu@users.noreply.github.com>
2021-11-02 21:39:16 -0400

    Update stepper_servo.ino

--- Kinetic Sculpture/Task1/stepper_servo.ino
+++ Kinetic Sculpture/Task1/stepper_servo.ino
@@ -1,12 +1,16 @@
 //Includes the Arduino Stepper Library
 #include <Stepper.h>
+//Custom ESP32 Servo library because Arduino does not have one
 #include <ESP32Servo.h>
 
+//create servo object
 Servo myservo;
+//position counter
 int pos = 0;
+//servo GPIO pin number
 int servoPin = 13;
 
-// Defines the number of steps per rotation
+// Defines the number of steps per rotation, full revolution is 2048 for our servos
 const int stepsPerRevolution = 2048;
 
 // Creates an instance of stepper class
@@ -24,17 +28,17 @@ void setup() {
   ESP32PWM::allocateTimer(2);
   ESP32PWM::allocateTimer(3);
   myservo.setPeriodHertz(50);    // standard 50 hz servo
+  //adjusts rotation range of servo
   myservo.attach(servoPin, 500, 2500); 
 }
 
 void loop() {
   // Rotate CW 
-  
   myStepper.step(stepsPerRevolution);
   Serial.println("CW");
   delay(1000);
 
-    // put your main code here, to run repeatedly:
+  // put your main code here, to run repeatedly:
   for (int i = 0; i <= 10; i += 1) { // goes from 0 degrees to 180 degrees
     // in steps of 1 degree
     myservo.write(pos);    // tell servo to go to position in variable 'pos'
@@ -43,6 +47,7 @@ void loop() {
 
   pos = pos + 10;
   
+  //if the servo has reached 180 return to the original starting position
   if (pos == 180) {
     pos = 0;
     myservo.write(pos);    
